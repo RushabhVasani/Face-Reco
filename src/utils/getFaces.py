768e52605a7918f6f861a34ae4efd6e244d15c01
pulkitvyas08 <e19cse284@bennett.edu.in>
2020-11-11 13:08:13 +0530

    Perform path joining with string concatenation

--- src/utils/getFaces.py
+++ src/utils/getFaces.py
@@ -1,10 +1,9 @@
 import sys
 import numpy as np
-from os import path
 
 import config
 
-deps_path = list(np.load(path.join(config.PATH, 'deps_path.npy')))
+deps_path = list(np.load(config.PATH + 'deps_path.npy'))
 sys.path = deps_path + [path for path in sys.path if path not in deps_path]
 
 from os import listdir, system
@@ -19,7 +18,7 @@ CSEL = '\33[7m'
 
 def getFaces(training=False, model_n=0):
 
-    path = path.join(config.PATH, 'roots/')
+    path = config.PATH + 'roots/'
     if training:
         if not model_n:
             print("\nFacerec will store 10 different models of your face to master your face.")
